e5bfffd570ece61b139f26c7fe605e04bfa164a1
Kendall Vargas <114007374+Kvargas2005@users.noreply.github.com>
2025-02-11 21:45:33 -0600

    Update WebDeveloper.js

--- src/app/components/Experience/WebDeveloper.js
+++ src/app/components/Experience/WebDeveloper.js
@@ -22,9 +22,9 @@ export default function HHC() {
 
 
             <div className="inline-flex flex-wrap gap-2 mt-4 bg-[#1C1C20] rounded-full p-2 max-w-full">
-                <span className="text-[#8E94BC] px-4 py-2 rounded-full text-sm md:text-base lg:text-lg">Php</span>
-                <span className="text-[#8E94BC] px-4 py-2 rounded-full text-sm md:text-base lg:text-lg">JavaScript</span>
-                <span className="text-[#8E94BC] px-4 py-2 rounded-full text-sm md:text-base lg:text-lg">MySQL</span>
+                <span className="text-[#8E94BC] px-4 py-2 rounded-full text-sm md:text-base lg:text-lg">ASP.Net</span>
+                <span className="text-[#8E94BC] px-4 py-2 rounded-full text-sm md:text-base lg:text-lg">Jquery</span>
+                <span className="text-[#8E94BC] px-4 py-2 rounded-full text-sm md:text-base lg:text-lg">SQL Server</span>
                 <span className="text-[#8E94BC] px-4 py-2 rounded-full text-sm md:text-base lg:text-lg">HTML</span>
                 <span className="text-[#8E94BC] px-4 py-2 rounded-full text-sm md:text-base lg:text-lg">CSS</span>
             </div>
